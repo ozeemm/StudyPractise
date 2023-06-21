ce121194412ab0950c93a1abb485c12b91cc3da1
stud <stud@istu.int>
2023-06-21 14:44:53 +0800

    Added two table rows and numbers bounds

--- PractiseTask/PractiseTask.cpp
+++ PractiseTask/PractiseTask.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <Windows.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -31,29 +32,48 @@ int main()
     vector<Student> students; // Массив студентов
 
     int maxScore = 0; // Максимальное количество баллов
+    int maxStudentPercent = 0; // Максимальный процент успеваемости среди студентов 
 
     cout << "Ввод данных:" << endl;
 
     // Ввод данных о лаб.работах
-    cout << "Количество лабораторных работ: "; cin >> labsNum;
-    cout << "Веса лабораторных работ: ";
+    do {
+        cout << "Количество лабораторных работ: "; cin >> labsNum;
+
+        if (labsNum <= 0)
+            cout << "Ошибка: количество лабораторных работ должно быть положительным числом" << endl;
+    } while (labsNum <= 0);
     labsWeight.resize(labsNum);
+    
+    cout << "Веса лабораторных работ: ";
     for (int i = 0; i < labsNum; i++) {
-        cin >> labsWeight[i];
+        do {
+            cin >> labsWeight[i];
+            if(labsWeight[i] <= 0)
+                cout << "Ошибка: вес лабораторной работы должнен быть положительным числом" << endl;
+        } while (labsWeight[i] <= 0);
+
         maxScore += 5 * labsWeight[i];
     }
+    
 
     // Ввод данных о студентах
     cout << "Количество студентов: "; cin >> studentsNum;
     students.resize(studentsNum);
+
     for (int i = 0; i < studentsNum; i++) {
         cout << "Имя студента: ";
         cin.ignore();
         getline(cin, students[i].name);
         cout << "Результаты его лабораторных работ: ";
         students[i].labResults.resize(labsNum);
-        for (int j = 0; j < labsNum; j++)
-            cin >> students[i].labResults[j];
+        for (int j = 0; j < labsNum; j++) {
+            do {
+                cin >> students[i].labResults[j];
+                if (students[i].labResults[j] <= 0)
+                    cout << "Ошибка: оценка лабораторной работы может быть от 0 до 5" << endl;
+            } while (students[i].labResults[j] < 0 || students[i].labResults[j] > 5);
+        }
     }
 
     // Подсчёт итогового результата студента и процента успеваемости
@@ -61,6 +81,8 @@ int main()
         for (int j = 0; j < labsNum; j++)
             students[i].totalResult += students[i].labResults[j] * labsWeight[j];
         students[i].percentResult = students[i].totalResult * 100 / maxScore;
+        if (students[i].percentResult > maxStudentPercent)
+            maxStudentPercent = students[i].percentResult;
     }
 
     // Проверка на автомат
@@ -86,29 +108,33 @@ int main()
     cout << "Вывод данных" << endl;
     cout.width(20); cout << "";
     for (int i = 0; i < labsNum; i++) {
-        cout.width(8); cout << "Лаба " << (i + 1);
+        cout << setw(8) << "Лаба " << (i + 1);
     }
-    cout.width(8); cout << "Итог";
-    cout.width(22); cout << "Процент успеваемости";
-    cout.width(9); cout << "Автомат";
-    cout.width(9); cout << "Допущен";
+    cout << setw(8) << "Итог";
+    cout << setw(22) << "Процент успеваемости";
+    cout << setw(9) << "Автомат";
+    cout << setw(9) << "Допущен";
     cout << endl;
 
     for (int i = 0; i < studentsNum; i++) 
     {
-        cout.width(20); cout << students[i].name;
+        cout << setw(20) << students[i].name;
         for (int j = 0; j < labsNum; j++) {
-            cout.width(8); cout << students[i].labResults[j];
+            cout << setw(8) << students[i].labResults[j];
         }
-        cout.width(8); cout << students[i].totalResult;
-        cout.width(22); cout << students[i].percentResult;
-        cout.width(9); cout << (students[i].isAutoExam ? "+" : "-");
-        cout.width(9); cout << (students[i].isAllowedToExam ? "+" : "-");
+        cout << setw(8) << students[i].totalResult;
+        cout << setw(22) << students[i].percentResult;
+        cout << setw(9) << (students[i].isAutoExam ? "+" : "-");
+        cout << setw(9) << (students[i].isAllowedToExam ? "+" : "-");
         cout << endl;
     }
 
     cout.width(20); cout << "Вес лабы";
     for (int i = 0; i < labsNum; i++) {
-        cout.width(8); cout << labsWeight[i];
+        cout << setw(8) << labsWeight[i];
     }
+    cout << endl;
+
+    cout <<"Макс. возм. сумма баллов: " << maxScore << endl;
+    cout << "Самая высокая успеваемость: " << maxStudentPercent << endl;
 }
